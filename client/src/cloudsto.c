b3a58e50ab8e2ad4b317607ab09924775c36c9bb
Bojie Li <bojieli@gmail.com>
2013-01-31 16:42:46 +0800

    base64 encoded input/output

--- client/src/cloudsto.c
+++ client/src/cloudsto.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "cloudsto.h"
+#include "base64.h"
 
 char sendbuf[MAXLEN];
 char *sendptr;
 char *recvbuf;
 char *errmsg;
 
-int cloudsto_post(char* path, int sendlen, char* sendbuf, char** recvbuf) {
+int cloudsto_post(char* path, int sendlen, char* sendbuf, int* recvlen, char** recvbuf) {
+#ifdef DEBUG
     printf("Sending %d bytes...\n", sendlen);
+#endif
     tcpclient client;
     tcpclient_create(&client, CLOUD_SERVER, CLOUD_PORT);
-    int recvlen;
+
     char *fullpath = malloc(strlen(path) + strlen(CLOUD_BASE) + 2);
     strcpy(fullpath, CLOUD_BASE);
     strcat(fullpath, path);
-    int errorno = http_post(&client, fullpath, sendlen, sendbuf, recvbuf, &recvlen);
+
+    int encoded_sendlen;
+    char *encoded_sendbuf = base64_encode(sendlen, sendbuf, &encode_sendlen);
+    int encoded_recvlen;
+    char *encoded_recvbuf;
+
+    int errorno = http_post(&client, fullpath, encoded_sendlen, encoded_sendbuf, encoded_recvbuf, &encoded_recvlen);
     free(fullpath);
+    *recvbuf = base64_decode(encoded_recvbuf, encoded_recvlen, recvlen);
+
     if (errorno) {
         printf("Network error: %d\n", errorno);
         if (*recvbuf && **recvbuf)
             printf("Error message: %s\n", *recvbuf);
         return 1;
     }
+#ifdef DEBUG
     printf("Received %d bytes\n", recvlen);
+#endif
     return 0;
 }
 
